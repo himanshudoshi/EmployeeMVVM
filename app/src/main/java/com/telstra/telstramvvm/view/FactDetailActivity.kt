dbcd47ffe1d85de38776cacdf0ef43fe2661fe84
himanshudoshi <himanshudoshi2001@gmail.com>
2020-05-06 20:32:38 +0530

    This is First Commit for Telstra Demo with MVVM

---
+++ app/src/main/java/com/telstra/telstramvvm/view/FactDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.telstra.telstramvvm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.telstra.telstramvvm.R
+
+class FactDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
